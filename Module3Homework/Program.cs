a78f165a2128a6ebc3df8789e4e4ba5c161e2ad0
kthedw <68110933+kthedw@users.noreply.github.com>
2020-10-16 17:40:16 +0300

    Add files via upload

---
+++ Module3Homework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module3Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Module3Homework/employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Module3_
+{
+    public class Employee
+    {
+        public int experience;
+        public int skillLevel;
+        public string firstName;
+        public string lastName;
+
+        public Employee(int experience, int skillLevel, string firstName, string lastName)
+        {
+            this.experience = experience;
+            this.skillLevel = skillLevel;
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+    }
+}
