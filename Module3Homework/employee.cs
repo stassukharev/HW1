43646883ec7daeb5b07c1c944532b28ef85043ba
kit1987 <68110933+kit1987@users.noreply.github.com>
2020-10-18 13:33:39 +0300

    13:33

--- Module3Homework/employee.cs
+++ Module3Homework/employee.cs
@@ -18,10 +18,10 @@ namespace Module3_
         public int getBonusPercent() {
             
             if (experience < 1) return 0;
-            if (experience < 5) return 5;
+            if (experience < 5) && (experience > 1) return 5;
             if ((experience => 5) && (skillLevel <3)) return 10;
             if ((experience > 5) && (skillLevel => 3) && (skillLevel <= 4)) return 15;
-            if ((experience > 5) && (skillLevel = 5)) return 20;
+            if ((experience > 5) && (skillLevel == 5)) return 20;
 
         }
     }
