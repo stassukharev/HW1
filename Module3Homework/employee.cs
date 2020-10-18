ff648f3eb53896341e8bf631e52caec750d9b617
kit1987 <68110933+kit1987@users.noreply.github.com>
2020-10-18 14:02:04 +0300

    14:01

--- Module3Homework/employee.cs
+++ Module3Homework/employee.cs
@@ -3,25 +3,28 @@ namespace Module3_
 {
     public class Employee
     {
-        public int experience;
-        public int skillLevel;
         public string firstName;
         public string lastName;
+        public int experience;
+        public int skillLevel;
+        
 
-        public Employee(int experience, int skillLevel, string firstName, string lastName)
+        public Employee(string firstName, string lastName, int experience, int skillLevel)
         {
-            this.experience = experience;
-            this.skillLevel = skillLevel;
-            this.firstName = firstName;
-            this.lastName = lastName;
+             this.firstName = firstName;
+             this.lastName = lastName;
+             this.experience = experience;
+             this.skillLevel = skillLevel;
+           
         }
         public int getBonusPercent() {
             
             if (experience < 1) return 0;
             if ((experience < 5) && (experience > 1)) return 5;
-            if ((experience => 5) && (skillLevel <3)) return 10;
-            if ((experience > 5) && (skillLevel => 3) && (skillLevel <= 4)) return 15;
+            if ((experience >= 5) && (skillLevel < 3)) return 10;
+            if ((experience > 5) && (skillLevel >= 3) && (skillLevel <= 4)) return 15;
             if ((experience > 5) && (skillLevel == 5)) return 20;
+            else return -1;
 
         }
     }
