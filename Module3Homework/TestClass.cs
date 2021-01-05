81139edad3014cc5f4b2ffa5edae7c1f1d305456
Stanislav Sukharev <stas.sukharev87@gmail.com>
2021-01-05 19:31:11 +0300

    Automation Tests Final

--- Module3Homework/TestClass.cs
+++ Module3Homework/TestClass.cs
@@ -13,19 +13,19 @@ namespace ConsoleApp_Employee
         {
 
             Employee emma = new Employee("Emma", "Watson", 3, 3);
-            Assert.IsTrue(emma.getBonusPercent() == 0);
+            Assert.IsFalse(emma.getBonusPercent() == 0);
 
             Employee alan = new Employee("Alan", "Rickman", 10, 5);
-            Assert.IsTrue(alan.getBonusPercent() == 0);
+            Assert.IsFalse(alan.getBonusPercent() == 0);
 
             Employee fiona = new Employee("Fiona", "Shaw", 7, 3);
-            Assert.IsTrue(fiona.getBonusPercent() == 0);
+            Assert.IsFalse(fiona.getBonusPercent() == 0);
 
             Employee daniel  = new Employee("Daniel", "Redcliff", 1, 1);
             Assert.IsTrue(daniel.getBonusPercent() == 0);
 
             Employee rupert = new Employee("Rupert", "Grint", 2, 4);
-            Assert.IsTrue(rupert.getBonusPercent() == 0);
+            Assert.IsFalse(rupert.getBonusPercent() == 0);
         }
     
         [Test]
@@ -36,13 +36,13 @@ namespace ConsoleApp_Employee
             Assert.IsTrue(emma.getBonusPercent() == 5);
 
             Employee alan = new Employee("Alan", "Rickman", 10, 5);
-            Assert.IsTrue(alan.getBonusPercent() == 5);
+            Assert.IsFalse(alan.getBonusPercent() == 5);
 
             Employee fiona = new Employee("Fiona", "Shaw", 7, 3);
-            Assert.IsTrue(fiona.getBonusPercent() == 5);
+            Assert.IsFalse(fiona.getBonusPercent() == 5);
 
             Employee daniel = new Employee("Daniel", "Redcliff", 1, 1);
-            Assert.IsTrue(daniel.getBonusPercent() == 5);
+            Assert.IsFalse(daniel.getBonusPercent() == 5);
 
             Employee rupert = new Employee("Rupert", "Grint", 2, 4);
             Assert.IsTrue(rupert.getBonusPercent() == 5);
@@ -54,19 +54,19 @@ namespace ConsoleApp_Employee
         {
 
             Employee emma = new Employee("Emma", "Watson", 3, 3);
-            Assert.IsTrue(emma.getBonusPercent() == 10);
+            Assert.IsFalse(emma.getBonusPercent() == 10);
 
             Employee alan = new Employee("Alan", "Rickman", 10, 5);
-            Assert.IsTrue(alan.getBonusPercent() == 10);
+            Assert.IsFalse(alan.getBonusPercent() == 10);
 
             Employee fiona = new Employee("Fiona", "Shaw", 7, 3);
-            Assert.IsTrue(fiona.getBonusPercent() == 10);
+            Assert.IsFalse(fiona.getBonusPercent() == 10);
 
             Employee daniel = new Employee("Daniel", "Redcliff", 1, 1);
-            Assert.IsTrue(daniel.getBonusPercent() == 10);
+            Assert.IsFalse(daniel.getBonusPercent() == 10);
 
             Employee rupert = new Employee("Rupert", "Grint", 2, 4);
-            Assert.IsTrue(rupert.getBonusPercent() == 10);
+            Assert.IsFalse(rupert.getBonusPercent() == 10);
 
         }
 
@@ -75,24 +75,45 @@ namespace ConsoleApp_Employee
         {
 
             Employee emma = new Employee("Emma", "Watson", 3, 3);
-            Assert.IsTrue(emma.getBonusPercent() == 15);
+            Assert.IsFalse(emma.getBonusPercent() == 15);
 
             Employee alan = new Employee("Alan", "Rickman", 10, 5);
-            Assert.IsTrue(alan.getBonusPercent() == 15);
+            Assert.IsFalse(alan.getBonusPercent() == 15);
 
             Employee fiona = new Employee("Fiona", "Shaw", 7, 3);
             Assert.IsTrue(fiona.getBonusPercent() == 15);
 
             Employee daniel = new Employee("Daniel", "Redcliff", 1, 1);
-            Assert.IsTrue(daniel.getBonusPercent() == 15);
+            Assert.IsFalse(daniel.getBonusPercent() == 15);
 
             Employee rupert = new Employee("Rupert", "Grint", 2, 4);
-            Assert.IsTrue(rupert.getBonusPercent() == 15);
+            Assert.IsFalse(rupert.getBonusPercent() == 15);
 
         }
 
         [Test]
         public void CheckExperienceMoreOrEqual5SkillEqual5()
+        {
+
+            Employee emma = new Employee("Emma", "Watson", 3, 3);
+            Assert.IsFalse(emma.getBonusPercent() == 20);
+
+            Employee alan = new Employee("Alan", "Rickman", 10, 5);
+            Assert.IsTrue(alan.getBonusPercent() == 20);
+
+            Employee fiona = new Employee("Fiona", "Shaw", 7, 3);
+            Assert.IsFalse(fiona.getBonusPercent() == 20);
+
+            Employee daniel = new Employee("Daniel", "Redcliff", 1, 1);
+            Assert.IsFalse(daniel.getBonusPercent() == 20);
+
+            Employee rupert = new Employee("Rupert", "Grint", 2, 4);
+            Assert.IsFalse(rupert.getBonusPercent() == 20);
+
+        }
+
+        [Test]
+        public void TestFailed()
         {
 
             Employee emma = new Employee("Emma", "Watson", 3, 3);
@@ -102,37 +123,15 @@ namespace ConsoleApp_Employee
             Assert.IsTrue(alan.getBonusPercent() == 20);
 
             Employee fiona = new Employee("Fiona", "Shaw", 7, 3);
-            Assert.IsTrue(fiona.getBonusPercent() == 20);
+            Assert.IsFalse(fiona.getBonusPercent() == 20);
 
             Employee daniel = new Employee("Daniel", "Redcliff", 1, 1);
-            Assert.IsTrue(daniel.getBonusPercent() == 20);
+            Assert.IsFalse(daniel.getBonusPercent() == 20);
 
             Employee rupert = new Employee("Rupert", "Grint", 2, 4);
-            Assert.IsTrue(rupert.getBonusPercent() == 20);
+            Assert.IsFalse(rupert.getBonusPercent() == 20);
 
         }
-
-        [Test]
-        public void Failed()
-        {
-
-            Employee emma = new Employee("Emma", "Watson", 50, 3);
-            Assert.IsTrue(emma.getBonusPercent() == 10);
-
-            Employee alan = new Employee("Alan", "Rickman", 10, 5);
-            Assert.IsTrue(alan.getBonusPercent() == 10);
-
-            Employee fiona = new Employee("Fiona", "Shaw", 7, 3);
-            Assert.IsTrue(fiona.getBonusPercent() == 10);
-
-            Employee daniel = new Employee("Daniel", "Redcliff", -1, 1);
-            Assert.IsTrue(daniel.getBonusPercent() == 10);
-
-            Employee rupert = new Employee("Rupert", "Grint", 114, 4);
-            Assert.IsTrue(rupert.getBonusPercent() == 10);
-
-        }
-
 
     }
 
